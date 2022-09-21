8c0e5c66ed83cab5d47e9bafea812876de3f68aa
blduan <1578770584@qq.com>
2022-09-21 21:20:52 +0800

    install nerd font and icon

---
+++ install_nerd_font.sh
@@ -0,0 +1,4 @@
+git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
+cd nerd-fonts
+git sparse-checkout add patched-fonts/JetBrainsMono
+git sparse-checkout add patched-fonts/FiraCode
---
+++ set_git_proxy.sh
@@ -0,0 +1,2 @@
+git config --global http.proxy http://127.0.0.1:41091
+git config --global https.proxy https://127.0.0.1:41091
