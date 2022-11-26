2ab4f9a7e06d0ee6dc2499b06e9d6eed1bd9a029
blduan <binlin.duan@foxmail.com>
2022-11-26 17:09:18 +0800

    auto change input method

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -38,3 +38,21 @@ autocmd({
     pattern = "*",
     callback = function() require("lint").try_lint() end,
 }) ]]
+
+-- 支持输入法切换
+if vim.fn.has("linux") == 1 then
+    local reservedIM1 = "xkb:us::eng"
+    local reservedIM2 = "xkb:us::eng"
+    autocmd({ "InsertEnter", "VimLeave" }, {
+        callback = function()
+            reservedIM1 = vim.trim(vim.fn.system("ibus engine"))
+            if reservedIM2 then vim.trim(vim.fn.system("ibus engine " .. reservedIM2)) end
+        end,
+    })
+    autocmd({ "InsertLeave", "VimEnter" }, {
+        callback = function()
+            reservedIM2 = vim.trim(vim.fn.system("ibus engine"))
+            if reservedIM1 then vim.trim(vim.fn.system("ibus engine " .. reservedIM1)) end
+        end,
+    })
+end
