13c31b19f83165d30fd7137ec48f313bac33b8d1
blduan <1578770584@qq.com>
2023-10-06 04:57:34 +0000

    修改git proxy

--- set_git_proxy.sh
+++ set_git_proxy.sh
@@ -1,2 +1,2 @@
-git config --global http.proxy http://192.168.62.1:7890
-git config --global https.proxy https://192.168.62.1:7890
+git config --global http.proxy http://192.168.2.100:7890
+git config --global https.proxy https://192.168.2.100:7890
