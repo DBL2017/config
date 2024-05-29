7467852350697fa1b858398c342f9e12bf69661c
blduan <blduan@nbjetron.com>
2024-05-29 14:38:01 +0800

    更新diagnostics与nvim-tree的配置

--- lazy_nvim_config/nvim/lua/extra/diagnostics.lua
+++ lazy_nvim_config/nvim/lua/extra/diagnostics.lua
@@ -3,16 +3,16 @@ local M = {}
 
 M.setup = function()
     -- replace the lsp info symbol
-    local signs = {
-        { name = "DiagnosticSignError", text = "E" },
-        { name = "DiagnosticSignWarn",  text = "W" },
-        { name = "DiagnosticSignHint",  text = "H" },
-        { name = "DiagnosticSignInfo",  text = "I" },
-    }
+    -- local signs = {
+    --     { name = "DiagnosticSignError", text = "E" },
+    --     { name = "DiagnosticSignWarn",  text = "W" },
+    --     { name = "DiagnosticSignHint",  text = "H" },
+    --     { name = "DiagnosticSignInfo",  text = "I" },
+    -- }
 
-    for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
+    -- for _, sign in ipairs(signs) do
+    --     vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+    -- end
 
     -- set the style of lsp info
     local config = {
@@ -21,7 +21,13 @@ M.setup = function()
         virtual_text = false,
         -- show signs
         signs = {
-            active = signs,
+            -- active = signs,
+            text = {
+                [vim.diagnostic.severity.ERROR] = "E",
+                [vim.diagnostic.severity.WARN] = "W",
+                [vim.diagnostic.severity.HINT] = "H",
+                [vim.diagnostic.severity.INFO] = "I"
+            }
         },
         update_in_insert = true,
         underline = true,
