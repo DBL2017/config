b546d52af4096773e38b1b4a75dcbcf23675fb9c
DBL2017 <1578770584@qq.com>
2024-11-28 16:50:50 +0000

    tab indent

--- lazy_nvim_config/nvim/lua/lsp/lspsaga.lua
+++ lazy_nvim_config/nvim/lua/lsp/lspsaga.lua
@@ -1,16 +1,16 @@
 return {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({
-            outline = {
-                close_after_jump = true,
-                win_width = 40,
-            }
-        })
-    end,
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter', -- optional
-        'nvim-tree/nvim-web-devicons',     -- optional
-    },
-
+	"nvimdev/lspsaga.nvim",
+	enabled = true,
+	config = function()
+		require("lspsaga").setup({
+			outline = {
+				close_after_jump = true,
+				win_width = 40,
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional
+		"nvim-tree/nvim-web-devicons", -- optional
+	},
 }
