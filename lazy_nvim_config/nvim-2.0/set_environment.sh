8a706128cc622d8f04c155578144fb3c8c7532d2
DBL2017 <1578770584@qq.com>
2025-09-27 10:33:09 +0800

    增加初始设置环境变量

--- lazy_nvim_config/nvim-2.0/set_environment.sh
+++ lazy_nvim_config/nvim-2.0/set_environment.sh
@@ -6,5 +6,8 @@ echo "export NVIM_LOG_FILE=/dev/null" >> ~/.bashrc
 echo "export DISPLAY=192.168.100.1:0.0" >> ~/.bashrc
 # 设置硅基流动deepseek key
 echo "export DEEPSEEK_API_KEY_S=<key>" >> ~/.bashrc
-
+# 设置KIMI KEY
+excho "export KIMI_API_KEY=<key>" >> ~/.bashrc
+# 设置DashScope的QWEN3 KEY
+echo "export QWEN3_CODER_PLUS_2025=<key>" >> ~/.bashrc
 
