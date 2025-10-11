76d7f36d87b856ff40842d64e11c39c87ededda9
duanbinlin <duanbinlin@tp-link.com.hk>
2025-10-11 19:51:10 +0800

    优化set_environment.sh脚本，解决shellcheck警告问题

--- lazy_nvim_config/nvim-2.0/set_environment.sh
+++ lazy_nvim_config/nvim-2.0/set_environment.sh
@@ -2,11 +2,17 @@
 
 # 安装python3虚拟环境
 sudo apt install python3-venv
-# 设置该环境变量作用在于解决lualine导致日志爆炸的问题
-echo "export NVIM_LOG_FILE=/dev/null" >> ~/.bashrc
-# 支持xclip远程复制功能，需要Windows上安装X11 Server，可以采用MobaXterm自带的
-echo "export DISPLAY=192.168.100.1:0.0" >> ~/.bashrc
-# 设置硅基流动deepseek key
-echo "export DEEPSEEK_API_KEY_S=<key>" >> ~/.bashrc
-
-
+{
+	# 设置该环境变量作用在于解决lualine导致日志爆炸的问题
+	export NVIM_LOG_FILE=/dev/null
+	# 支持xclip远程复制功能，需要Windows上安装X11 Server，可以采用MobaXterm自带的
+	export DISPLAY=192.168.100.1:0.0
+	# 设置硅基流动deepseek key
+	export DEEPSEEK_API_KEY_S="<key>"
+	# 设置Kimi环境变量
+	export KIMI_API_KEY="<key>"
+	# 设置Dashscope部署的QWEN3的KEY
+	export QWEN3_CODER_PLUS_2025="<key>"
+	# 设置环境变量用于表示为工作电脑
+	export RUN_ENVIRONMENT="WORK"
+} >> ~/.bashrc
