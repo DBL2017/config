a4df0dd0f54ba5976c0e6628ab35a122e4069531
duanbinlin <duanbinlin@tp-link.com.hk>
2025-11-27 14:17:50 +0800

    1.阻止配置文件发生变化自动重新加载ui，因为这会导致ui卡死

--- lazy_nvim_config/nvim-2.0/lua/config/lazy.lua
+++ lazy_nvim_config/nvim-2.0/lua/config/lazy.lua
@@ -222,7 +222,7 @@ require("lazy").setup({
     },
     change_detection = {
         -- automatically check for config file changes and reload the ui
-        enabled = true,
+        enabled = false,
         notify = true, -- get a notification when changes are found
     },
     performance = {
